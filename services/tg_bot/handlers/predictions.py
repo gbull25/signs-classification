2464bbce48f2bbdb515e6ce44cb73234fe636ec6
gbull25 <gbulygin@yandex.ru>
2024-01-13 18:36:05 +0300

    add SIFT to telegram bot

--- services/tg_bot/handlers/predictions.py
+++ services/tg_bot/handlers/predictions.py
@@ -1,3 +1,4 @@
+import emoji
 from io import BytesIO
 from aiogram import Router, F, Bot
 from aiogram.types import Message, InputMediaPhoto
@@ -11,7 +12,8 @@ router = Router()
 @router.message(F.media_group_id, F.content_type.in_({'photo'}))
 async def handle_albums(message: Message, album: list[Message]):
     media_group = []
-    class_pred = []
+    hog_pred = []
+    sift_pred = []
     for msg in album:
         if msg.photo:
             file_id = msg.photo[-1].file_id
@@ -20,15 +22,22 @@ async def handle_albums(message: Message, album: list[Message]):
             io = BytesIO()
             await bot.download(msg.photo[-1], destination=io)
             im = io.getvalue()
-            data = preprocessing.predict_hog_image(im)
-            class_pred.append(data['sign class'])
+            data_hog = preprocessing.predict_hog_image(im)
+            hog_pred.append(data_hog['sign class'])
+            data_sift = preprocessing.predict_sift_image(im)
+            sift_pred.append(data_sift['sign class'])
 
+    data_class = {'hog_class': hog_pred, 'sift_class': sift_pred}
+
+    # Возвращаем альбом для удобства чтения результатов классификации
     await message.answer_media_group(media_group) 
-
+    # Возвращаем предсказания
     i = 0
-    for sign in class_pred:
+    for hog, sift in zip(data_class['hog_class'], data_class['sift_class']):
         i += 1
-        await message.reply(f"На фотографии номер {i} знак точно {sign} класса") 
+        await message.reply(f'На фотографии номер {i}:\n'
+                           f'*HOG SVM* считает, что знак {hog} класса,\n'
+                           f'*SIFT SVM* считает, что знак {sift} класса\.') 
 
 
 # Хэндлер на одну фотографию
@@ -37,5 +46,7 @@ async def predict_image(message: Message):
     io = BytesIO()
     await bot.download(message.photo[-1], destination=io)
     im = io.getvalue()
-    data = preprocessing.predict_hog_image(im)
-    await message.reply(f"Этот знак точно {data['sign class']} класса")
+    data_hog = preprocessing.predict_hog_image(im)
+    data_sift = preprocessing.predict_sift_image(im)
+    await message.reply(f'*HOG SVM* считает, что этот знак {data_hog["sign class"]} класса,\n'
+                        f'*SIFT SVM* считает, что этот знак {data_sift["sign class"]} класса\.')
