677fdbb56e7fba1c07e829e9e175460157881573
onthebox <onthebox@yandex.ru>
2024-02-13 20:37:04 +0300

    Added upload_album callback test

--- services/tg_bot/tests/test_handlers.py
+++ services/tg_bot/tests/test_handlers.py
@@ -40,5 +40,28 @@ async def test_upload_photo():
 
     assert hasattr(answer_message, "photo"), "Received reply has no attribute 'photo'."
     assert hasattr(answer_message, "caption"), "Received reply has no attribute 'caption'."
-    assert isinstance(answer_message.photo, FSInputFile), f"Recieved reply 'photo' content of a wrong type: {type(answer_message.photo)}."
-    assert answer_message.caption == "Прошу, ваша тестовая картинка готова\!", "Recieved reply 'caption' has invalid content."
+    assert isinstance(answer_message.photo, FSInputFile), f"Recieved reply's 'photo' attribute content of a wrong type: {type(answer_message.photo)}."
+    assert answer_message.caption == "Прошу, ваша тестовая картинка готова\!", "Recieved reply's 'caption' attribute  has invalid content."
+
+
+@pytest.mark.asyncio
+async def test_upload_album():
+    requester = MockedBot(CallbackQueryHandler(menu.send_album))
+
+    # Make test data to request n_photos in album
+    n_photos = np.random.randint(1, 6)
+    data = f'photos_{n_photos}'
+
+    callback_query = CALLBACK_QUERY.as_object(data=data, message=MESSAGE.as_object())
+    calls = await requester.query(callback_query)
+    answer_message = calls.send_media_group.fetchone()
+
+    assert hasattr(answer_message, "media"), "Recieved reply has no attribute 'media'."
+    assert isinstance(answer_message.media, list), f"Recieved reply's 'media' attribute content of a wrong type: {type(answer_message.media)}."
+    assert len(answer_message.media) == n_photos, f"Recieved reply's 'media' attribute content list of a wrong length: {len(answer_message.media)} != {n_photos}."
+    assert all([isinstance(x, dict) for x in answer_message.media]), f"Recieved reply's 'media' attribute's elements of a wrong type."
+    assert all([isinstance(x["media"], FSInputFile) for x in answer_message.media]), f"Recieved reply's 'media' attribute contains media of a wrong type."
+    assert answer_message.media[0]["caption"] == f"Прошу, ваши {n_photos} тестовых картинок готовы\\!", "Recieved reply has wrong caption."
+
+
+
