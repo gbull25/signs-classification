1f660d1d9fbefeff90f0a0586760f57c80dfa78e
onthebox <onthebox@yandex.ru>
2024-02-28 17:26:55 +0300

    Docker compose WIP

--- services/tg_bot/handlers/predictions.py
+++ services/tg_bot/handlers/predictions.py
@@ -1,14 +1,14 @@
 from io import BytesIO
 
+# import aiohttp
+import requests
 from aiogram import Bot, F, Router
 from aiogram.types import InputMediaPhoto, Message
-import requests
 
 #from services.model import preprocessing
 
 router = Router()
 
-#_APP_ADDRESS = "http://app:5001/predict"
 
 # Хэндлер на альбом фотографий
 @router.message(F.media_group_id, F.content_type.in_({'photo'}))
@@ -16,34 +16,57 @@ async def handle_albums(message: Message, album: list[Message], bot: Bot):
     media_group = []
     hog_pred = []
     sift_pred = []
-    for msg in album:
+    cnn_pred = []
+
+    # aiohttp WIP
+    # request_data = aiohttp.FormData()
+
+    for i, msg in enumerate(album):
         if msg.photo:
+
             file_id = msg.photo[-1].file_id
             media_group.append(InputMediaPhoto(media=file_id))
 
             io = BytesIO()
             await bot.download(msg.photo[-1], destination=io)
             im = io.getvalue()
+            
+            # aiohttp WIP
+            # request_data.add_field('file',
+            #    im,
+            #    filename=f'file_{i}',
+            #    content_type='image/png')
 
-            #data_hog = preprocessing.predict_hog_image(im)
-            data_hog = requests.post("http://app:5001/predict_many_signs_hog", files=im)
+            # HOG
+            data_hog = requests.post("http://sign_classifier:80/predict/sign_hog", files={'file': im}).json()
             hog_pred.append((data_hog['sign_class'], data_hog['sign_description']))
 
-            #data_sift = preprocessing.predict_sift_image(im)
-            data_sift = requests.post("http://app:5001/predict_many_signs_sift", files=im)
+            # SIFT
+            data_sift = requests.post("http://sign_classifier:80/predict/sign_sift", files={'file': im}).json()
             sift_pred.append((data_sift['sign_class'], data_sift['sign_description']))
 
-    data_class = {'hog_class': hog_pred, 'sift_class': sift_pred}
+            # CNN
+            data_cnn = requests.post("http://sign_classifier:80/predict/sign_cnn", files={'file': im}).json()
+            cnn_pred.append((data_cnn['sign_class'], data_cnn['sign_description']))
+
+    # aiohttp WIP
+    # async with aiohttp.ClientSession() as session:
+    #     res = await session.post("http://sign_classifier:80/predict/signs_cnn", data=request_data)
+
+    # await message.answer(res.status)
+
+    data_class = {'hog_class': hog_pred, 'sift_class': sift_pred, 'cnn_class': cnn_pred}
 
     # Возвращаем альбом для удобства чтения результатов классификации
     await message.answer_media_group(media_group)
     # Возвращаем предсказания
     i = 0
-    for hog, sift in zip(data_class['hog_class'], data_class['sift_class']):
+    for hog, sift, cnn in zip(data_class['hog_class'], data_class['sift_class'], data_class['cnn_class']):
         i += 1
         await message.reply(f'На фотографии номер {i}:\n'
                             f'*HOG SVM* считает, что знак {hog[0]} класса \(_{hog[1]}_\),\n'
-                            f'*SIFT SVM* считает, что знак {sift[0]} класса \(_{sift[1]}_\)\.')
+                            f'*SIFT SVM* считает, что знак {sift[0]} класса \(_{sift[1]}_\)\n'
+                            f'*CNN* считает, что знак {cnn[0]} класса \(_{cnn[1]}_\)\.')
 
 
 # Хэндлер на одну фотографию
@@ -52,9 +75,12 @@ async def predict_image(message: Message, bot: Bot):
     io = BytesIO()
     io = await bot.download(message.photo[-1], destination=io)
     im = io.getvalue()
-    data_hog = requests.post("http://app:5001/predict_one_sign_hog", files=im)
-    data_sift = requests.post("http://app:5001/predict_one_sign_sift", files=im)
+    data_hog = requests.post("http://sign_classifier:80/predict/sign_hog", files={'file': im}).json()
+    data_sift = requests.post("http://sign_classifier:80/predict/sign_sift", files={'file': im}).json()
+    data_cnn = requests.post("http://sign_classifier:80/predict/sign_cnn", files={'file': im}).json()
     await message.reply(f'*HOG SVM* считает, что этот знак '
                         f'{data_hog["sign_class"]} класса \(_{data_hog["sign_description"]}_\),\n'
                         f'*SIFT SVM* считает, что этот знак '
-                        f'{data_sift["sign_class"]} класса \(_{data_sift["sign_description"]}_\)\.')
+                        f'{data_sift["sign_class"]} класса \(_{data_sift["sign_description"]}_\)\.'
+                        f'*CNN* считает, что этот знак '
+                        f'{data_cnn["sign_class"]} класса \(_{data_cnn["sign_description"]}_\)\.')
